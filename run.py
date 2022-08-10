c5117f574f2bc7042edcbb41d207a66befa0ae03
JackDay94 <Jack94day@gmail.com>
2022-08-10 19:38:39 +0000

    Add backstory to introduction

--- run.py
+++ run.py
@@ -34,6 +34,16 @@ def introduction():
                    ,╔╣▓╬▀▀▀╖,
                  ╫▓▒╢╣╬@▒░╬╬╣▓Γ
     """)
+    print()
+    print("Long ago, a powerful king named Mazar reigned undisputed.\n")
+    print("Until one day, he was betrayed by his son and his reign ended.\n")
+    print("Before his death, Mazar built a great tomb and stashed his most prized treasure.\n")
+    print("It's wherabouts laid secret for untold years...\n")
+    print("Until suddenly, rumours began to spread of its supposed location.\n")
+    print("Many went in search for the great treasures within.\n")
+    print("But not a single soul has returned to tell the tale.\n")
+    print("Will you enter the tomb and succeed where others have failed...?\n")
+    print("Do you have what it takes to claim the fabled Treasure of Mazar?\n")
 
 
 introduction()
