8b4af3af1012fc0c1320ff6b9055a5963eb30f5c
JackDay94 <Jack94day@gmail.com>
2022-08-10 20:53:05 +0000

    Add start_game function and add basic functionality

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import sys
 def introduction():
     """
     Opens the game with the title screen and introduction
-    to the story of the game
+    to the story of the game.
     """
     print()
     print("""
@@ -44,6 +44,25 @@ def introduction():
     print("But not a single soul has returned to tell the tale.\n")
     print("Will you enter the tomb and succeed where others have failed...?\n")
     print("Do you have what it takes to claim the fabled Treasure of Mazar?\n")
+    start_game()
+
+
+def start_game():
+    """
+    Lets the user choose to start the game and enter
+    their name before beginning.
+    """
+    start_choice = input("Are you ready to start your adventure? (Yes/No)\n")
+    if start_choice.lower() == "yes":
+        player_name = input("What is your name?\n")
+        print(f"Welcome {player_name}. Your adventure awaits!\n")
+    elif start_choice.lower() == "no":
+        print("Very well... Please take your time.")
+        time.sleep(5)
+        introduction()
+    else:
+        print("Please type Yes or no!")
+        start_game()
 
 
 introduction()
